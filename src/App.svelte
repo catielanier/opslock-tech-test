b1114de6597e06cde2b71f02966ad024c45e6e0c
Corey Lanier <iam@coreylanier.com>
2021-03-28 15:54:19 -0400

    added pseudo-code to explain logic

--- src/App.svelte
+++ src/App.svelte
@@ -57,20 +57,26 @@
 	];
 
 	const addShift = (shift: IShift): void => {
+		// set the start and end times of the shift into ints
 		const start: number = parseInt(shift.start, 10);
 		const end: number = parseInt(shift.end, 10);
+		// create an empty to house each hour of the shift, include start and end times by default
 		const shiftHours: number[] = [start, end];
+		// loop through the start and end times by one hour, and add each hour to the array
 		for (let i = start + 100; i < end; i += 100) {
 			shiftHours.push(i);
 		}
+		// run the verification of the shift
 		verifyShift(shift, shiftHours);
 	};
 
+	// recursive function to verify that each shift the user is working doesn't conflict with the selected shift
 	const verifyShift = (
 		shift: IShift,
 		shiftHours: number[],
 		index: number = 0
 	): void => {
+		// if all user shifts have been checked, add the selected shift to the shifts array, then sort it by shift time
 		if (index === usershifts.length) {
 			const shifts = [...usershifts];
 			shifts.push(shift);
@@ -80,25 +86,30 @@
 					parseInt(b.start, 10)
 			);
 			usershifts = shifts;
+			// reset the error message in case it was previously displayed
 			error = null;
+			// empty return to end the function, prevent endless loop
 			return;
 		}
+		// set the start and end times of the currently compared user shift as ints
 		const startShift: number = parseInt(
 			usershifts[index].start,
 			10
 		);
 		const endShift: number = parseInt(usershifts[index].end, 10);
+		// loop through each hour of the selected shift to see if it falls within the user's existing shift
 		for (let i = 0; i < shiftHours.length; i++) {
 			if (
 				shiftHours[i] > startShift &&
 				shiftHours[i] < endShift
 			) {
-				console.log("not a valid shift");
+				// if it does, set the error message and empty return to end the function.
 				error =
 					"This shift falls within a shift you are already working.";
 				return;
 			}
 		}
+		// if the loop through doesn't find a conflict, rerun the recursive function on the next index of the user's shifts
 		verifyShift(shift, shiftHours, index + 1);
 	};
 
