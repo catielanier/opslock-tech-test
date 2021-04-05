c3d0439d611935872eae2c70d307f3f3f4cc841a
Corey Lanier <iam@coreylanier.com>
2021-04-04 23:37:19 -0400

    quick circular button fix

--- src/components/Shifts.svelte
+++ src/components/Shifts.svelte
@@ -1,41 +1,43 @@
 <script lang="ts">
-	interface IShift {
-		start: string;
-		end: string;
-	}
-	export let shifts: IShift[];
-	export let deleteShift: Function;
+  interface IShift {
+    start: string;
+    end: string;
+  }
+  export let shifts: IShift[];
+  export let deleteShift: Function;
 </script>
 
 <style>
-	ul {
-		list-style-type: none;
-		list-style-position: inside;
-	}
+  ul {
+    list-style-type: none;
+    list-style-position: inside;
+  }
 
-	button {
-		border-radius: 50%;
-		padding: 10px;
-		background: red;
-		color: white;
-		cursor: pointer;
-	}
+  button {
+    border-radius: 50%;
+    background: red;
+    color: white;
+    cursor: pointer;
+    width: 35px;
+    height: 35px;
+    text-align: center;
+  }
 </style>
 
 <div class="shifts">
-	<h2>Your Shifts:</h2>
-	<ul>
-		{#each shifts as shift, index}
-			<li>
-				{shift.start}
-				-
-				{shift.end}
-				<button
-					class="del"
-					on:click={() => {
-						deleteShift(index);
-					}}>X</button>
-			</li>
-		{/each}
-	</ul>
+  <h2>Your Shifts:</h2>
+  <ul>
+    {#each shifts as shift, index}
+      <li>
+        {shift.start}
+        -
+        {shift.end}
+        <button
+          class="del"
+          on:click={() => {
+            deleteShift(index);
+          }}>X</button>
+      </li>
+    {/each}
+  </ul>
 </div>
